19c8576e969ae3a5c4dd297d1d34cb2bedaab342
Alioune TRIANDE <cheicktriande@gmail.com>
2025-02-12 00:05:11 +0000

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'TracaInnoFaso',
+  webDir: 'www'
+};
+
+export default config;
