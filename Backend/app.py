48981a6ecb7a1a7b84bd0ee250caed148319e54c
Alioune TRIANDE <cheicktriande@gmail.com>
2025-02-17 11:00:44 +0000

    Second commit

--- Backend/app.py
+++ Backend/app.py
@@ -7,8 +7,8 @@ import io
 app = Flask(__name__)
 CORS(app)
 
-LABEL_WIDTH = int(105 * 11.81)  # 105 mm en pixels
-LABEL_HEIGHT = int(74 * 11.81)  # 74 mm en pixels
+LABEL_WIDTH = int(74 * 11.81)  # 74 mm en pixels
+LABEL_HEIGHT = int(105 * 11.81)  # 105 mm en pixels
 
 @app.route('/generate-label', methods=['POST'])
 def generate_label():
@@ -26,21 +26,38 @@ def generate_label():
     draw = ImageDraw.Draw(label)
 
     try:
-        font = ImageFont.truetype("arial.ttf", 30)
+        font = ImageFont.truetype("arial.ttf", 40)
     except IOError:
         font = ImageFont.load_default()
 
-    draw.text((20, 20), f"GTIN: {gtin}", font=font, fill="black")
-    draw.text((20, 60), f"Content GTIN: {content_gtin}", font=font, fill="black")
-    draw.text((20, 100), f"Batch: {batch}", font=font, fill="black")
-    draw.text((20, 140), f"EXP: {expiry_date}", font=font, fill="black")
-    draw.text((20, 180), f"Quantity: {quantity}", font=font, fill="black")
+    draw.text((30, 60), f"GTIN", font=font, fill="black")
+    draw.text((30, 130), f"{gtin}", font=font, fill="black", stroke_width=2, stroke_fill="black")
+    draw.text((590, 60), f"Content GTIN", font=font, fill="black")
+    draw.text((590, 130), f"{content_gtin}", font=font, fill="black", stroke_width=2, stroke_fill="black")
+    draw.text((30, 220), f"Batch", font=font, fill="black")
+    draw.text((30, 290), f"{batch}", font=font, fill="black", stroke_width=2, stroke_fill="black")
+    draw.text((30, 380), f"EXPIRATION", font=font, fill="black")
+    draw.text((30, 450), f"{expiry_date}", font=font, fill="black", stroke_width=2, stroke_fill="black")
+    draw.text((590, 220), f"QUANTITY", font=font, fill="black")
+    draw.text((590, 290), f"{quantity}", font=font, fill="black", stroke_width=2, stroke_fill="black")
+    
+    # Dessiner un trait horizontal au milieu de l'étiquette
+    draw.line([(10, 650), (860, 650)], fill="black", width=5)
+
+    draw.text((550, 900), f"(01) {gtin}", font=font, fill="black")
+    draw.text((550, 950), f"(10) {expiry_date}", font=font, fill="black")
+    draw.text((550, 1000), f"(17) {batch}", font=font, fill="black")
+    draw.text((550, 1050), f"(02) {content_gtin}", font=font, fill="black")
+    draw.text((550, 1100), f"(37) {quantity}", font=font, fill="black")
+   
+ 
+ 
 
     dm_data = f"GTIN:{gtin};CONTENT GTIN:{content_gtin};BATCH:{batch};EXP:{expiry_date};QTY:{quantity};;"
     encoded = encode(dm_data.encode('utf-8'))
     dm_image = Image.frombytes('RGB', (encoded.width, encoded.height), encoded.pixels)
 
-    dm_size = 100
+    dm_size = 400
     dm_image = dm_image.resize((dm_size, dm_size))
 
     label.paste(dm_image, (20, LABEL_HEIGHT - dm_size - 20))
