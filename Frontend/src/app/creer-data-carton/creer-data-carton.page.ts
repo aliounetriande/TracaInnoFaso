ab695a48c2f1650326875a3790d4b269150de053
Alioune TRIANDE <cheicktriande@gmail.com>
2025-02-18 09:45:28 +0000

    Les telechargements se font en pdf

--- Frontend/src/app/creer-data-carton/creer-data-carton.page.ts
+++ Frontend/src/app/creer-data-carton/creer-data-carton.page.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormBuilder, FormGroup } from '@angular/forms';
 import { FormsModule } from '@angular/forms';
+import jspdf from 'jspdf';
 import { IonContent, IonHeader, IonTitle, IonToolbar, IonItem, IonLabel, IonButton, IonInput } from '@ionic/angular/standalone';
 import { HttpClientModule, HttpClient } from '@angular/common/http';
 import { saveAs } from 'file-saver';
@@ -42,9 +43,37 @@ export class CreerDataCartonPage {
   }
 
   downloadLabel(): void {
-    if (this.labelUrl) {
-      saveAs(this.labelUrl, "etiquette.png");
+    // if (this.labelUrl) {
+    //   saveAs(this.labelUrl, "etiquette.png");
+    // }
+    if (!this.labelUrl) {
+      console.error("Aucune image n'est disponible pour le téléchargement en PDF");
+      return;
     }
+    
+    //créé un élément image pour charger l'image
+    const img = new Image();
+    img.src = this.labelUrl;
+
+   //créer le document PDF 
+    img.onload = () => {
+
+      const doc = new jspdf({
+        orientation: img.width >img.height ? 'landscape' : 'portrait',
+        unit: 'px',
+        format: [img.width, img.height]
+      });
+
+      //Ajouter l'image dans le PDF
+      doc.addImage(img, 'PNG', 0, 0, img.width, img.height);
+
+      //sauvegarder le pdf
+      doc.save('Etiquette-carton.pdf');
+  };
+  img.onerror = (error) => {
+    console.error('Erreur lors du chargement de l\'image pour le PDF ', error);
+    
+  }
   }
 
 }
